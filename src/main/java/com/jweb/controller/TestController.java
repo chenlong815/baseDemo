470e5c680a38d6639e35899d347d011331605f50
281910885@qq.com <cl155213>
2016-08-02 10:01:58 +0800

    加入Service层

--- src/main/java/com/jweb/controller/TestController.java
+++ src/main/java/com/jweb/controller/TestController.java
@@ -1,13 +1,14 @@
-package com.jweb.controller;/**
- * Created by chenlong on 2016/5/11.
- */
+package com.jweb.controller;
 
 import com.jweb.common.StateMsg;
+import com.jweb.service.TestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * 测试
@@ -15,11 +16,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class TestController {
+    @Resource
+    TestService testService;
+
     @ResponseBody
     @RequestMapping(value = "test",method = RequestMethod.GET)
     public String test(ModelMap map){
         String jsonStr;
-        map.addAttribute("test","test by ChenLong");
+        String msg;
+        msg=testService.testService();
+        map.addAttribute("test",msg);
         jsonStr= StateMsg.toJson(1, map);
         return jsonStr;
     }
---
+++ src/main/java/com/jweb/service/TestService.java
@@ -0,0 +1,14 @@
+package com.jweb.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2016/8/2.
+ */
+@Service
+public class TestService {
+
+    public String testService(){
+        return "test Service";
+    }
+}
