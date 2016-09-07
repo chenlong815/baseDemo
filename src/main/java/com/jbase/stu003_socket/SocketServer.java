0394a7459377696947207f39ee106efc4a4d495d
陈龙 <281910885@qq.com>
2016-09-07 11:07:52 +0800

    添加Socket相关知识点

--- src/main/java/com/jbase/stu003_socket/SocketServer.java
+++ src/main/java/com/jbase/stu003_socket/SocketServer.java
@@ -5,32 +5,18 @@ import java.net.*;
 
 public class SocketServer{
     public static void main(String[] args) throws IOException{
-
         ServerSocket server=new ServerSocket(5678);
-
         Socket client=server.accept();
-
         BufferedReader in=new BufferedReader(new InputStreamReader(client.getInputStream()));
-
         PrintWriter out=new PrintWriter(client.getOutputStream());
-
         while(true){
-
             String str=in.readLine();
-
             System.out.println(str);
-
             out.println("has receive.");
-
             out.flush();
-
             if(str.equals("end"))
-
-                break;
-
+            break;
         }
-
         client.close();
-
     }
 }
